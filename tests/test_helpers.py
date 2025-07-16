d56924590bfe67aa3b274aaf64cebbdd1184fad9
barts <bartsteensma@outlook.com>
2025-07-16 18:33:34 +0200

    fix: unittest

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -1,5 +1,4 @@
 from time import sleep
-from typing import override
 from unittest import TestCase
 from unittest.mock import patch
 
@@ -13,27 +12,20 @@ class TestCounter(TestCase):
         cnt: the current count.
     """
 
-    cnt: int
-
-    @override
-    def setUp(self):
-        self.cnt = 0
-
     @patch("sys.stdout.write")
     def test_counter(self, patched_write):
-        def assert_write(actual: str):
-            assert actual == f"\r{self.cnt}/10"
+        counter = Counter(goal=10)
 
+        def assert_write(actual: str):
+            assert actual == f"\r{counter.value}/10"
         patched_write.side_effect = assert_write
 
-        counter = Counter(goal=10)
         for i in range(10):
-            self.cnt += 1
-            counter.increment()
+            counter.value += 1
 
 
 if __name__ == "__main__":
     counter = Counter(goal=10, prefix="Counting: ", suffix=" seconds")
     for i in range(10):
-        counter.increment()
+        counter.value += 1
         sleep(1)
